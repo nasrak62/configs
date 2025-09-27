3aa24a60928b91c66e6fb09f7ebf57402216a122
SaarZaid <saar.zaidfunden@pipl.com>
2025-09-27 21:37:53 +0300

    added lazyvim

---
+++ lazy-nvim/lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+--
+
+local keymap = vim.keymap
+
+keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", { desc = "tmux navigation left" })
+keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", { desc = "tmux navigation down" })
+keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", { desc = "tmux navigation up" })
+keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", { desc = "tmux navigation right" })
